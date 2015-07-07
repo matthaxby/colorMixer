87527378a3394f503a768717bd54e12479e91709
Matt Haxby <matthaxby@gmail.com>
2015-07-07 15:10:07 -0600

    functional! Needs style

--- public/client.js
+++ public/client.js
@@ -1,16 +1,38 @@
 var mix = document.getElementById('mix-color')
 var colorOne = document.getElementsByName('color-one')
 var colorTwo = document.getElementsByName('color-two')
+var newColor = document.getElementById('mixed')
+var finalColor = ''
+var firstColor = []
+var secondColor = []
+var mixedColor = []
 
 for (var i=0; i < colorOne.length; i++) {
   colorOne[i].addEventListener('click', function(e) {
-    console.log(e.target.dataset.colors.split(','))
+    firstColor = e.target.dataset.colors.split(',')
+    firstColor.forEach(function(val, index) {
+      firstColor[index] = parseInt(val)
+    })
+    console.log(firstColor)
   })
 }
 
 
 for (var i=0; i < colorTwo.length; i++) {
   colorTwo[i].addEventListener('click', function(e) {
-    console.log(e.target.dataset.colors.split(','))
+    secondColor = e.target.dataset.colors.split(',')
+    secondColor.forEach(function(val, index) {
+      secondColor[index] = parseInt(val)
+    })
+    console.log(secondColor)
+    console.log(firstColor[0] + secondColor[0])
+    mixedColor = [(firstColor[0] + secondColor[0])/2, (firstColor[1] + secondColor[1])/2, (firstColor[2] + secondColor[2])/2, 1]
+    finalColor = 'rgba(' + mixedColor.join(',') + ')'
+    newColor.style.backgroundColor = finalColor
+    console.log(finalColor)
   })
 }
+
+
+
+console.log(mixedColor)
