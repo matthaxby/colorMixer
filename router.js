a8bda74d5a08f61cb54f4e731932369990380c5f
Matt Haxby <matthaxby@gmail.com>
2015-07-07 12:07:13 -0600

    can add new colors!

--- router.js
+++ router.js
@@ -18,14 +18,11 @@ routes.addRoute('/colors', (req, res, url) => {
       res.end(template)
     })
   }
-
   if (req.method === 'POST') {
     var data = ''
-
     req.on('data', function (chunk) {
       data += chunk
     })
-
     req.on('end', function () {
       var color = qs.parse(data)
       colors.insert(color, function (err, doc) {
@@ -36,5 +33,18 @@ routes.addRoute('/colors', (req, res, url) => {
     })
   }
 })
+
+routes.addRoute('/public/*', (req, res, url) => {
+  res.setHeader('Content-Type', mime.lookup(req.url))
+  fs.readFile('.' + req.url, function(err, file) {
+    if (err) {
+      res.setHeader('Content-Type', 'text/html')
+      res.end('404')
+    }
+    res.end(file)
+  })
+})
+
+
 
 module.exports = routes
